d9ef45cd8a4ae3b8c59525c11b54406cefe736ba
thirumurugan sivalingam <thiru@synlian.net>
2024-03-30 13:08:16 +0530

    testing done

--- sdk/src/index.js
+++ sdk/src/index.js
@@ -1,6 +1,7 @@
 import FormComponent from "./Components/FormComp";
 
 import SignMessageComponent from "./Components/Sign";
+import Greet from "./Components/Greet";
 
 
-export {FormComponent, SignMessageComponent}
+export {FormComponent, SignMessageComponent,Greet}
