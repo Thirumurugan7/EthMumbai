80104a67e8ec7699adb97381b01c23d1bed9c9ad
vishalmet <vishalmet003@gmail.com>
2024-03-31 07:48:41 +0530

    zk

--- frontend/src/components/External.jsx
+++ frontend/src/components/External.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React from "react";
+import Navbar from "../components/Navbar";
 
 const ExternalLink = () => {
   return (
-    <div>
-      <iframe
-        src="https://anon-aadhaar-rosy.vercel.app/"
-        title="External Link"
-        width="100%"
-        height="600px"
-        frameBorder="0"
-      ></iframe>
+    <div classname=" gradient-container bg-black h-screen grotesk-font">
+      <div className=" text-black">
+        <Navbar />
+      </div>
+      <div className="  mx-6 pt-[10%] items-center">
+        <div className=" flex justify-center">
+          <div
+            classname=""
+            style={{ width: "30%", height: "250px", overflow: "hidden" }}
+          >
+            <iframe
+              classname=" "
+              src="https://anon-aadhaar-rosy.vercel.app/"
+              title="External Link"
+              width="100%"
+              height="100%"
+              frameBorder="0"
+              scrolling="no" // Disable scrolling on the iframe
+            ></iframe>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
